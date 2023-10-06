22cafbc9af0d8f9467064bc03e05adc8fd940d1b
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-10-05 23:22:11 -0400

    described CRUD for model("Lesson")

--- education/urls.py
+++ education/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from education.apps import EducationConfig
-from education.views import CourseViewSet
+from education.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = EducationConfig.name
 
@@ -9,5 +10,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
-] + router.urls
+                  path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+                  path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+                  path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-detail'),
+                  path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+                  path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+              ] + router.urls
